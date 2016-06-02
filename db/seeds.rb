ec1ebcca3de78d9ee85b82789e5660d8d8022445
polegithub <tschengkong@163.com>
2016-06-02 12:11:22 +0800

    [ADD] add initial sample code from Ruby on Rails Tutorial
    notes: including some test code

---
+++ db/seeds.rb
@@ -0,0 +1,43 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Users
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+9.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
+end
+
+# Microposts
+users = User.order(:created_at).take(6)
+5.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..5]
+followers = users[3..6]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should redirect following when not logged in" do
+    get :following, id: @user
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get :followers, id: @user
+    assert_redirected_to login_url
+  end
+
+
+end
